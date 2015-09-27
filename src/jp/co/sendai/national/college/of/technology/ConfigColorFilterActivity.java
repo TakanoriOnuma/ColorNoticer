4c1a4e80aa1751c4137079d4c3a541f6087f095c
Takanori Onuma <s1000607@sendai-nct.jp>
2015-09-27 15:48:21 +0900

    カラーフィルタ設定アクティビティの作成
    カラーフィルタを設定するアクティビティを作成した。
    メインからの遷移、Backキーからの戻りも実装した。
    ただ遷移を実行する場所は暫定で書いた。
    いずれNavigationDrawerを理解して適切な場所で実行するように書き換える
    
    Signed-off-by: Takanori Onuma <s1000607@sendai-nct.jp>

--- src/jp/co/sendai/national/college/of/technology/ConfigColorFilterActivity.java
+++ src/jp/co/sendai/national/college/of/technology/ConfigColorFilterActivity.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.util.List;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.Bitmap.Config;
 import android.graphics.ImageFormat;
@@ -12,34 +11,13 @@ import android.hardware.Camera;
 import android.hardware.Camera.Parameters;
 import android.hardware.Camera.Size;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.KeyEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
 
-public class MainActivity extends ActionBarActivity
-        implements NavigationDrawerFragment.NavigationDrawerCallbacks,
-            SurfaceHolder.Callback, Camera.PreviewCallback {
-
-    /**
-     * Fragment managing the behaviors, interactions and presentation of the navigation drawer.
-     */
-    private NavigationDrawerFragment mNavigationDrawerFragment;
-
-    /**
-     * Used to store the last screen title. For use in {@link #restoreActionBar()}.
-     */
-    private CharSequence mTitle;
+public class ConfigColorFilterActivity extends Activity
+        implements SurfaceHolder.Callback, Camera.PreviewCallback {
 
     private SurfaceView mSvFacePreview;
     private SurfaceHolder mSurfaceHolder;
@@ -55,16 +33,7 @@ public class MainActivity extends ActionBarActivity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        mNavigationDrawerFragment = (NavigationDrawerFragment)
-                getSupportFragmentManager().findFragmentById(R.id.navigation_drawer);
-        mTitle = getTitle();
-
-        // Set up the drawer.
-        mNavigationDrawerFragment.setUp(
-                R.id.navigation_drawer,
-                (DrawerLayout) findViewById(R.id.drawer_layout));
+        setContentView(R.layout.activity_config_colorfilter);
 
         // SurfaceViewでカメラが利用できるように設定
         mSvFacePreview = (SurfaceView)findViewById(R.id.FacePreview);
@@ -72,109 +41,6 @@ public class MainActivity extends ActionBarActivity
         mSurfaceHolder.addCallback(this);
 
         mOverLay = (OverLayView)findViewById(R.id.OverLayView);
-    }
-
-    @Override
-    public void onNavigationDrawerItemSelected(int position) {
-        // update the main content by replacing fragments
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        fragmentManager.beginTransaction()
-                .replace(R.id.container, PlaceholderFragment.newInstance(position + 1))
-                .commit();
-    }
-
-    public void onSectionAttached(int number) {
-        Intent intent;
-        switch (number) {
-        case 1:
-            mTitle = getString(R.string.title_section1);
-            break;
-        case 2:
-            // 応急処置の切り替え（実際は別なところからやると思う）
-            mTitle = getString(R.string.title_section2);
-            surfaceDestroyed(mSurfaceHolder);
-
-            intent = new Intent(MainActivity.this, ConfigColorFilterActivity.class);
-            startActivity(intent);
-            break;
-        case 3:
-            mTitle = getString(R.string.title_section3);
-            break;
-        }
-    }
-
-    public void restoreActionBar() {
-        ActionBar actionBar = getSupportActionBar();
-        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
-        actionBar.setDisplayShowTitleEnabled(true);
-        actionBar.setTitle(mTitle);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        if (!mNavigationDrawerFragment.isDrawerOpen()) {
-            // Only show items in the action bar relevant to this screen
-            // if the drawer is not showing. Otherwise, let the drawer
-            // decide what to show in the action bar.
-            getMenuInflater().inflate(R.menu.main, menu);
-            restoreActionBar();
-            return true;
-        }
-        return super.onCreateOptionsMenu(menu);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-            textView.setText(Integer.toString(getArguments().getInt(ARG_SECTION_NUMBER)));
-            return rootView;
-        }
-
-        @Override
-        public void onAttach(Activity activity) {
-            super.onAttach(activity);
-            ((MainActivity) activity).onSectionAttached(
-                    getArguments().getInt(ARG_SECTION_NUMBER));
-        }
     }
 
     private Size getOptimalPreviewSize(List<Size> sizes, int w, int h) {
@@ -300,5 +166,16 @@ public class MainActivity extends ActionBarActivity
             mCamera.release();
             mCamera = null;
         }
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        // BACKキーが入力されたらサブアクティビティを終了する
+        if(keyCode == KeyEvent.KEYCODE_BACK) {
+            surfaceDestroyed(mSurfaceHolder);
+            finish();
+            return true;
+        }
+        return false;
     }
 }
