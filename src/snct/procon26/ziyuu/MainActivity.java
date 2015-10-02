f8ba48df4be33c608b9de063240365450e172c99
Takanori Onuma <s1000607@sendai-nct.jp>
2015-10-02 10:12:00 +0900

    各種の機能をメインで実行するようにした
    各種設定画面でON／OFF設定したのち、ONなら起動するようにした。
    両方動作すると重いが、もう少し早くできそうな気はする。
    
    Signed-off-by: Takanori Onuma <s1000607@sendai-nct.jp>

--- src/snct/procon26/ziyuu/MainActivity.java
+++ src/snct/procon26/ziyuu/MainActivity.java
@@ -3,19 +3,25 @@ package snct.procon26.ziyuu;
 import java.io.IOException;
 import java.util.List;
 
-import snct.procon26.ziyuu.colortransfar.ColorTransfar;
-import snct.procon26.ziyuu.imageviewer.ImageViewer;
-
 import jp.co.sendai.national.college.of.technology.R;
+import snct.procon26.ziyuu.colortransfar.ColorFilter;
+import snct.procon26.ziyuu.colortransfar.ColorTransfar;
+import snct.procon26.ziyuu.colortransfar.ColorValueTransfar;
+import snct.procon26.ziyuu.imageviewer.ColorInfoDrawer;
+import snct.procon26.ziyuu.imageviewer.ImageViewer;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.Bitmap.Config;
 import android.graphics.ImageFormat;
+import android.graphics.Point;
+import android.graphics.Rect;
 import android.hardware.Camera;
 import android.hardware.Camera.Parameters;
 import android.hardware.Camera.Size;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.widget.DrawerLayout;
@@ -45,7 +51,7 @@ public class MainActivity extends ActionBarActivity
      */
     private CharSequence mTitle;
 
-    private ColorTransfar mColorTransfar;
+    private ColorTransfar mColorTransfar = new ColorTransfar();
 
     private SurfaceView mSvFacePreview;
     private SurfaceHolder mSurfaceHolder;
@@ -57,6 +63,9 @@ public class MainActivity extends ActionBarActivity
     private int[]  mImageData;
     private Bitmap mBitmap;
     private ImageViewer mOverLay;
+    private ColorInfoDrawer mColorInfoDrawer = null;
+
+    private SharedPreferences mPref;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -78,6 +87,9 @@ public class MainActivity extends ActionBarActivity
         mSurfaceHolder.addCallback(this);
 
         mOverLay = (ImageViewer)findViewById(R.id.OverLayView);
+
+        // リファレンスマネージャの取得
+        mPref = PreferenceManager.getDefaultSharedPreferences(this);
     }
 
     @Override
@@ -231,6 +243,15 @@ public class MainActivity extends ActionBarActivity
 
             mColorTransfar.decodeYUV420SP(frame, data, mPreviewSize.width, mPreviewSize.height);
 
+            if(mColorInfoDrawer != null) {
+                // 色情報の取得
+                Point pos = mOverLay.getCursorPoint();
+                Rect  viewRect = mOverLay.getViewRect();
+                int color = ColorTransfar.getColor(data, mPreviewSize.width, mPreviewSize.height,
+                        mPreviewSize.width * pos.x / viewRect.right, mPreviewSize.height * pos.y / viewRect.bottom);
+                mColorInfoDrawer.setColorInfo(color);
+            }
+
             // Bitmapに描画して、OverLayに再描画を促す
             mBitmap.setPixels(frame, 0, mPreviewSize.width,
                     0, 0, mPreviewSize.width, mPreviewSize.height);
@@ -291,8 +312,35 @@ public class MainActivity extends ActionBarActivity
                         0, 0, mPreviewSize.width, mPreviewSize.height);
                 mOverLay.setBitmap(mBitmap);
 
-                // 色変換クラスの作成
-                mColorTransfar = new ColorTransfar();
+                // 各種設定の読み込み
+                if(mPref.getBoolean("isColorValueTransfarFunction", false)) {
+                    ColorValueTransfar colorValueTransfar = new ColorValueTransfar();
+                    colorValueTransfar.setRedRate(mPref.getInt("redColor", 50));
+                    colorValueTransfar.setGreenRate(mPref.getInt("greenColor", 50));
+                    colorValueTransfar.setBlueRate(mPref.getInt("blueColor", 50));
+                    mColorTransfar.setColorValueTransfar(colorValueTransfar);
+                }
+                else {
+                    mColorTransfar.setColorValueTransfar(null);
+                }
+                if(mPref.getBoolean("isFlashingFunction", false)) {
+                    ColorFilter colorFilter = new ColorFilter();
+                    colorFilter.setHueStart(mPref.getInt("hueStart", 0));
+                    colorFilter.setHueEnd(mPref.getInt("hueEnd", 0));
+                    colorFilter.setSaturation(mPref.getInt("saturation", 50));
+                    mColorTransfar.setColorFilter(colorFilter);
+                }
+                else {
+                    mColorTransfar.setColorFilter(null);
+
+                }
+                if(mPref.getBoolean("isColorInfoFunction", false)) {
+                    mColorInfoDrawer = new ColorInfoDrawer();
+                }
+                else {
+                    mColorInfoDrawer = null;
+                }
+                mOverLay.setColorInfoDrawer(mColorInfoDrawer);
 
                 // フレームバッファを追加
                 mCamera.setPreviewCallbackWithBuffer(this);
