c63f9f55ca4e95fa6744d44bd2012a5f8a8a17c3
Takanori Onuma <s1000607@sendai-nct.jp>
2015-10-03 11:57:12 +0900

    初回起動時にメニューが開かれるバグを直した
    サンプルコードでわざわざ初回起動時にはメニューを出したままに
    するようにしていたため、そのフラグを最初からtrueにした。
    そもそもその辺のコードを全部消してしまえば問題ない気もするが、
    なぜか上手くいかなかったので取りあえず最初からtrueにして対処した。
    
    Signed-off-by: Takanori Onuma <s1000607@sendai-nct.jp>

--- src/snct/procon26/ziyuu/NavigationDrawerFragment.java
+++ src/snct/procon26/ziyuu/NavigationDrawerFragment.java
@@ -66,8 +66,10 @@ public class NavigationDrawerFragment extends Fragment {
 
         // Read in the flag indicating whether or not the user has demonstrated awareness of the
         // drawer. See PREF_USER_LEARNED_DRAWER for details.
+        // このフラグは初期起動にメニュバーを開いたままにするかなので、
+        // 取りあえず最初からtrueにして閉じるようにした。
         SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(getActivity());
-        mUserLearnedDrawer = sp.getBoolean(PREF_USER_LEARNED_DRAWER, false);
+        mUserLearnedDrawer = sp.getBoolean(PREF_USER_LEARNED_DRAWER, true);
 
         if (savedInstanceState != null) {
             mCurrentSelectedPosition = savedInstanceState.getInt(STATE_SELECTED_POSITION);
